9b5154e15e6eb7d460249abfd7d66117ca26db09
Asias-ara <74496043+Asias-ara@users.noreply.github.com>
2022-05-22 21:37:51 +0900

    Look 벡터 오류 수정
    Look 벡터 오류 수정

--- SSUClient/Object.cpp
+++ SSUClient/Object.cpp
@@ -796,13 +796,23 @@ XMFLOAT3 CGameObject::GetRight()
 void CGameObject::SetLook(XMFLOAT3 xmf3Look)
 {
 	m_xmf3Look = Vector3::Normalize(xmf3Look);
-	m_xmf3Right = Vector3::CrossProduct(GetUp(), GetLook(), true);
-	m_xmf3Up = Vector3::CrossProduct(GetLook(), GetRight(), true);
+	m_xmf3Right = Vector3::CrossProduct(m_xmf3Up, m_xmf3Look, true);
+	m_xmf3Up = Vector3::CrossProduct(m_xmf3Look, m_xmf3Right, true);
 
-	m_xmf4x4ToParent._11 = m_xmf3Right.x; m_xmf4x4ToParent._12 = m_xmf3Right.y; m_xmf4x4ToParent._13 = m_xmf3Right.z;
-	m_xmf4x4ToParent._21 = m_xmf3Up.x; m_xmf4x4ToParent._22 = m_xmf3Up.y; m_xmf4x4ToParent._23 = m_xmf3Up.z;
-	m_xmf4x4ToParent._31 = m_xmf3Look.x; m_xmf4x4ToParent._32 = m_xmf3Look.y; m_xmf4x4ToParent._33 = m_xmf3Look.z;
-	m_xmf4x4ToParent = Matrix4x4::Multiply(XMMatrixScaling(m_xmf3Scale.x, m_xmf3Scale.y, m_xmf3Scale.z), m_xmf4x4ToParent);
+	m_xmf4x4ToParent._11 = m_xmf3Scale.x * m_xmf3Right.x; 
+	m_xmf4x4ToParent._12 = m_xmf3Scale.y * m_xmf3Right.y; 
+	m_xmf4x4ToParent._13 = m_xmf3Scale.z * m_xmf3Right.z;
+
+	m_xmf4x4ToParent._21 = m_xmf3Scale.x * m_xmf3Up.x; 
+	m_xmf4x4ToParent._22 = m_xmf3Scale.y * m_xmf3Up.y; 
+	m_xmf4x4ToParent._23 = m_xmf3Scale.z * m_xmf3Up.z;
+
+	m_xmf4x4ToParent._31 = m_xmf3Scale.x * m_xmf3Look.x; 
+	m_xmf4x4ToParent._32 = m_xmf3Scale.y * m_xmf3Look.y;
+	m_xmf4x4ToParent._33 = m_xmf3Scale.z * m_xmf3Look.z;
+	// m_xmf4x4ToParent = Matrix4x4::Multiply(XMMatrixScaling(m_xmf3Scale.x, m_xmf3Scale.y, m_xmf3Scale.z), m_xmf4x4ToParent);
+
+	UpdateTransform(NULL);
 }
 
 void CGameObject::MoveStrafe(float fDistance)
